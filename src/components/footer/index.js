3230e77f710d69726e2159d7fb092d53c5425c97
yazantafesh <yazantafesh1@gmail.com>
2021-08-08 13:41:14 +0300

    done without deploy

---
+++ src/components/footer/index.js
@@ -0,0 +1,16 @@
+// import React from 'react';
+
+// class Footer extends React.Component {
+//   render() {
+//     return <footer>&copy; 2018</footer>;
+//   }
+// }
+
+// export default Footer;
+import React from 'react'
+
+function Footer() {
+  return <footer>&copy; 2018</footer>;
+}
+
+export default Footer
---
+++ src/components/header/index.js
@@ -0,0 +1,25 @@
+// import React from 'react';
+
+// class Header extends React.Component {
+//   render() {
+//     return (
+//       <header>
+//         <h1>RESTy</h1>
+//       </header>
+//     );
+//   }
+// }
+
+// export default Header;
+
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+        <h1>RESTy</h1>
+      </header>
+  )
+}
+
+export default Header
